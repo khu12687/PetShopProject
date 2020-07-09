7ad0150e1e69a1beb6228f7c9de05cd2b210c53d
khu12687 <61855040+khu12687@users.noreply.github.com>
2020-07-09 11:27:51 +0900

    마이바티스 스프링 설정

---
+++ src/com/pet/controller/admin/AdminController.java
@@ -0,0 +1,24 @@
+package com.pet.controller.admin;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.pet.model.admin.Admin;
+import com.pet.model.admin.AdminService;
+
+@Controller
+public class AdminController {
+	@Autowired(required = false)
+	private AdminService adminService;
+	
+	@RequestMapping(value="/admin/test", method=RequestMethod.GET)
+	public ModelAndView loginCheck(Admin admin) {
+		Admin result=adminService.loginCheck(admin);
+		System.out.println("결과 id : "+result.getId());
+		return null;
+	}
+	
+}
